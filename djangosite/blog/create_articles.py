338f3863147e87cde6cbc51e6a7d6f6783f48bbe
joe <joe94113@gmail.com>
2021-03-12 20:55:57 +0800

    DEBUG FORM CAN NOT POST

--- djangosite/blog/create_articles.py
+++ djangosite/blog/create_articles.py
@@ -3,13 +3,14 @@ from .models import Tag, Articles
 
 
 class edit_articles_form(forms.Form):
-    def __init__(self,id):
+    def __init__(self, id):  # 設至初始值
         super(edit_articles_form, self).__init__()
         self.fields['title'].initial = Articles.objects.filter(id=id)[0].title
         self.fields['content'].initial = Articles.objects.filter(id=id)[0].content
         self.fields['tags'].initial = Articles.objects.filter(id=id)[0].tags.all()
-    title = forms.CharField(required=True,widget=forms.TextInput(attrs={'class': "form-control"}))
-    content = forms.CharField(required=True,widget=forms.Textarea(attrs={'class': "form-control"}))
+
+    title = forms.CharField(required=True, widget=forms.TextInput(attrs={'class': "form-control"}))
+    content = forms.CharField(required=True, widget=forms.Textarea(attrs={'class': "form-control"}))
     tags = forms.ModelMultipleChoiceField(queryset=Tag.objects.all(),
                                           widget=forms.SelectMultiple(attrs={'class': "selectpicker"}))
 
@@ -18,4 +19,4 @@ class create_articles_form(forms.Form):
     title = forms.CharField(required=True, widget=forms.TextInput(attrs={"class": "form-control"}))
     content = forms.CharField(required=True, widget=forms.Textarea(attrs={"class": "form-control"}))
     tags = forms.ModelMultipleChoiceField(queryset=Tag.objects.all(),
-                                  widget=forms.SelectMultiple(attrs={'class': "selectpicker"}))
+                                          widget=forms.SelectMultiple(attrs={'class': "selectpicker"}))
